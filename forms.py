243c5e57c38e2b0fb476ee1df05f207b6cdebba6
JJ-Cham <jcham@smith.edu>
2026-07-01 16:08:16 -0400

    Uncomment and update RegistrationForm fields

--- forms.py
+++ forms.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, url_for, flash, redirect
-# from forms import RegistrationForm
-from flask_behind_proxy import FlaskBehindProxy
+# from flask import Flask, render_template, url_for, flash, redirect
+# # from forms import RegistrationForm
+# from flask_behind_proxy import FlaskBehindProxy
+
+# class RegistrationForm(FlaskForm):
+#     username = StringField('Username',
+#                            validators=[DataRequired(), Length(min=2, max=20)])
+#     email = StringField('Email',
+#                         validators=[DataRequired(), Email()])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     confirm_password = PasswordField('Confirm Password',
+#                                      validators=[DataRequired(), EqualTo('password')])
+#     submit = SubmitField('Sign Up')
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
