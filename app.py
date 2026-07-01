87981f6dcd99236a3e5679e74526d4cd617c7d23
JJ-Cham <jcham@smith.edu>
2026-07-01 14:49:55 -0400

    Integrate SQLAlchemy and define User model
    Added SQLAlchemy integration and User model to app.py.

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import git
 from flask import Flask, render_template, url_for, flash, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-# @app.route("/update_server", methods=["POST"])
-# def update_server():
-#     return "OK", 200
+    
 @app.route("/update_server", methods=['POST'])
 def webhook():
     if request.method == 'POST':
@@ -18,3 +17,18 @@ def webhook():
         return 'Updated PythonAnywhere successfully', 200
     else:
         return 'Wrong event type', 400
+        
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+  db.create_all()
